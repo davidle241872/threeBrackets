30289b6b30d804930f6899f46c49db054afa3912
COR Coding <corcoding@Coding-14.local>
2024-03-21 11:48:37 -0400

    made a button for the winner

---
+++ bracket.js
@@ -0,0 +1,36 @@
+// let player1 = document.getElementById("player1") 
+// let player2 = document.getElementById("player2");
+
+//     let game1={
+//     player1: 10,
+//     player2: 6
+
+//      }
+//     if(game1.player1 > game1.player2){
+//         player2.style.color = "blue"
+//     }else if(game1.player1 < game1.player2){
+//         player1.style.color = "blue"
+//     } else {
+//         console.log("It was a tie")
+//     }
+
+// let player3 = document.getElementById("player3")
+// let player4 = document.getElementById("player4");
+
+//         let game2={
+//         player3: 10,
+//         player4: 6
+
+//          }
+//         if(game2.player3 > game2.player4){
+//             player4.style.color = "blue"
+//         }else if(game2.player4 < game2.player3){
+//             player3.style.color = "blue"
+//         } else {
+//             console.log("It was a tie")
+//         }
+
+function congrats(){
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
